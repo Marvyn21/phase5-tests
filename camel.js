8bb667e2dc11979be177d303ac7891b80defd7d3
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-07-19 15:50:44 +0300

    added Fizzbuzz

--- camel.js
+++ camel.js
@@ -1,15 +1,48 @@
-function camelCase(str){
-    const array = str.split(" ")
-    console.log(array)
-    for (i = 0; i < array.length; i++){
-        console.log(array[i].charAt(0).toUpperCase() + array[i].slice(1));
+// function camelCase(str){
+//     const array = str.split(" ").map((word) => {
+//         return word.slice(0,1).toUpperCase() + word.slice(1)
+//     })
+//     console.log(array.join(""))
+//     return array.join("")
+// }
+
+// camelCase("camel case word")
+
+
+// // area of a circle
+// function areaCircle(r){
+//     console.log(Math.PI * (Math.pow(r,2)))
+//     return Math.PI * Math.pow(r,2)
+// }
+// areaCircle(2)
+
+// // multiples 
+// function multiples(m, n){
+//     let arr = []
+//     for(let i=1; i<=m; i++){
+//         arr.push(i * n)
+//     }
+//     console.log(arr)
+//     return arr
+// }
+// multiples(4, 5)
+
+
+function fizzBuzz(n){
+    for(i=1; i <= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        }
+        else if(i % 3 === 0){
+            console.log("Fizz")
+        }
+        else if(i % 5 === 0){
+            console.log("Buzz")
+        }
+        else{
+            console.log(i)
+        }
     }
 }
 
-camelCase("camel case word")
-
-function areaCircle(r){
-    console.log(Math.PI * (Math.pow(r,2).toFixed(3)))
-    // return Math.PI * Math.pow(r,2).toFixed(3)
-}
-areaCircle(2)
+fizzBuzz(75)
